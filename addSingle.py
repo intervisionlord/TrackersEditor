955b105a7518834d36160ff636d1e5a1fc2f5cf1
intervisionlord <ads-net@mail.ru>
2023-08-28 17:12:38 +0300

    Добавление параметра

--- addSingle.py
+++ addSingle.py
@@ -1,25 +1,25 @@
 from torrent_edit import edit as edit_torrent
 import re
-# from sys import argv
-import torrent_edit
+from sys import argv
 from shutil import copy as filecopy
 from yaml import full_load as yamlload
 
-trackers_file = 'trackers.txt'
-# torrents_dir = argv[1]
-tracker_list = []
-torrent_path = input('Enter a full path to single torrent: ')
+# torrent_file = argv[1]
+# torrent_path = input('Enter a full path to single torrent: ')
+trackers_list_file = 'trackers.txt'
+trackers_list = []
+torrent_path = argv[1]
 torrents_list = [torrent_path]
 
 with open('_config.yaml', 'r') as conffile:
     config = yamlload(conffile)
 
-with open(trackers_file, 'r') as f:
+with open(trackers_list_file, 'r') as f:
     trackers = f.readlines()
     for urls in trackers:
-        tracker_list.append(re.sub('\n', '', urls))
+        trackers_list.append(re.sub('\n', '', urls))
 
-edit_torrent(torrents_list, tracker_list)
+edit_torrent(torrents_list, trackers_list)
 
 print(f'Copying {torrent_path} to {config["upload_dir"]}')
 filecopy(torrent_path, config['upload_dir'])
