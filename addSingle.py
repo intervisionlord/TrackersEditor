6e5e39c7506cde9dd17042535ea26e03ea71048d
intervisionlord <ads-net@mail.ru>
2023-08-28 17:22:15 +0300

    Работа с конфигом

--- addSingle.py
+++ addSingle.py
@@ -3,23 +3,30 @@ import re
 from sys import argv
 from shutil import copy as filecopy
 from yaml import full_load as yamlload
+from os import path as ospath
 
-# torrent_file = argv[1]
-# torrent_path = input('Enter a full path to single torrent: ')
+
 trackers_list_file = 'trackers.txt'
 trackers_list = []
 torrent_path = argv[1]
 torrents_list = [torrent_path]
 
-with open('_config.yaml', 'r') as conffile:
-    config = yamlload(conffile)
+def config() -> dict:
+    if ospath.exists('_config.yaml'):
+        config_file = '_config.yaml'
+    else:
+        config_file = 'config.yaml'
+
+    with open(config_file, 'r') as conffile:
+        config_file = yamlload(conffile)
+    return config_file
 
 with open(trackers_list_file, 'r') as f:
     trackers = f.readlines()
     for urls in trackers:
-        trackers_list.append(re.sub('\n', '', urls))
+            trackers_list.append(re.sub('\n', '', urls))
 
 edit_torrent(torrents_list, trackers_list)
 
-print(f'Copying {torrent_path} to {config["upload_dir"]}')
-filecopy(torrent_path, config['upload_dir'])
+print(f'Copying {torrent_path} to {config()["upload_dir"]}')
+filecopy(torrent_path, config()['upload_dir'])
