6ce20c3c1def307904bbf56b3f5dbfe05b8b42aa
intervisionlord <ads-net@mail.ru>
2023-08-28 13:47:43 +0300

    удаление интерактивного ввода

--- addTrackers.py
+++ addTrackers.py
@@ -2,25 +2,28 @@
 import re
 from subprocess import run as runcommand
 import os
+from sys import argv
 
 trackers_file = 'trackers.txt'
-input_dir = ''
-output_dir = ''
+torrents_dir = argv[1]
 tracker_list = []
+torrents_list = []
 
 with open(trackers_file, 'r') as f:
     trackers = f.readlines()
     for urls in trackers:
         tracker_list.append(re.sub('\n', '', urls))
 
-torrent_file = input('File: ')
+for files in os.listdir(torrents_dir):
+    torrents_list.append(os.path.abspath(files))
 
-for i in tracker_list:
-    # os.exec(f'transmission-edit -a {i} {torrent_file}')
-    runcommand([
-        'transmission-edit',
-        f'-a {i}',
-        os.path.abspath(torrent_file)
-        ])
+print(torrents_list)
 
-print(tracker_list)
+for torrent in torrents_list:
+    for url in tracker_list:
+        print(f'trying to edit {(torrent)}')
+        runcommand([
+            'transmission-edit',
+            f'-a {url}',
+            torrent,
+            ])
